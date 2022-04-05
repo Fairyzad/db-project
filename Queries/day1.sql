cdc3d74ccf7bba6b440e3b64bdd755bba639f6fa
peri <parizat@live.com>
2022-04-05 10:55:01 -0700

    Restricting Rows with Where clause And,Or

--- Queries/day1.sql
+++ Queries/day1.sql
@@ -37,6 +37,68 @@ SELECT * FROM JOBS;
 
 SELECT * FROM LOCATIONS;
 
+/*
+ Only Getting Unique value from the result
+ for example,only get unique first name from employees table
+ */
+
+SELECT DISTINCT FIRST_NAME
+FROM EMPLOYEES;
+
+-- Distinct keyword uses for remove duplicate
+--Get only unique lasts names
+--This is looking for people with unique first name last name,full name
+SELECT DISTINCT FIRST_NAME,LAST_NAME
+FROM EMPLOYEES;
+
+--Restricting rows in the result using where clause
+
+--COMPARISON Operators =, > >=, < <=, !=, <>
+
+--LOGICAL Operators: AND OR
+
+--DISPLAY THE Employee information for the employee with first_name Ellen
+-- In SQL We use single quote for String.
+SELECT * FROM EMPLOYEES
+WHERE FIRST_NAME = 'Ellen';--Ellen != ellen, values are case sensitive
+
+--Display the Employee First_Name, Last Name, salary
+--for the employee with salary 11000
+
+SELECT  FIRST_NAME,LAST_NAME,SALARY
+FROM EMPLOYEES
+WHERE SALARY = 11000;
+
+--Display the Employee First_Name, Last Name, salary
+--for the employee with salary 4800 and First_Name David
+-- Logical AND
+SELECT  FIRST_NAME,LAST_NAME,SALARY
+FROM EMPLOYEES
+WHERE FIRST_NAME= 'David'AND SALARY= 4800;
+
+--Display the Employee First_Name, Last Name, salary
+--for the employee with salary 4800 or First_Name David
+-- Logical OR
+SELECT  FIRST_NAME,LAST_NAME,SALARY
+FROM EMPLOYEES
+WHERE FIRST_NAME= 'David'OR SALARY= 4800;
+
+
+--Display the Employee that make more than 5000 and less than 12000
+-- Logical OR
+SELECT * FROM EMPLOYEES
+WHERE SALARY>5000 AND SALARY <12000;
+
+--Display the Employee that have JOB_ID of
+--AD_VP
+--AD_ASST
+--FI_ACCOUNT
+--AC_ACCOUNT
+SELECT * FROM EMPLOYEES
+WHERE JOB_ID ='AD_VP'
+  OR JOB_ID='FI_ACCOUNT'
+  OR JOB_ID='AD_ASST'
+  OR JOB_ID='AC_ACCOUNT';
 
 
 
